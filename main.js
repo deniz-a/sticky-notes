fb3245881787fef07b9a336efeae013975a30cf4
Deniz Akşimşek <d.aksimsek1@outlook.com>
2019-07-07 18:08:09 +0300

    use relative paths for imports

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue/dist/vue.esm.browser.js'
 
-import App from '/components/App.js'
+import App from './components/App.js'
 
 new Vue({
   render: h => h(App),
