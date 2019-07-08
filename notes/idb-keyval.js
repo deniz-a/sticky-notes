e4c4ce6c4a6a9aca68434fbde2fbcfe359083451
Deniz Akşimşek <d.aksimsek1@outlook.com>
2019-07-08 13:02:23 +0300

    add idb-keyval store

---
+++ notes/idb-keyval.js
@@ -0,0 +1,36 @@
+import { get, set, del, keys, Store } from 'https://cdn.jsdelivr.net/npm/idb-keyval@3/dist/idb-keyval.mjs'
+
+import { checkNote } from './note.js'
+
+export default function IdbKeyvalNoteRepo() {
+  const store = new Store('notes-db', 'notes')
+
+  return {
+
+    getNotes() {
+      return keys(store).then(keys => Promise.all(
+        keys.map(k => get(k, store))))
+    },
+
+    getNoteById(id) {
+      return get(id, store)
+    },
+
+    saveNote(note) {
+      if (checkNote(note)) {
+        note.id = String(Math.random())
+        return set(note.id, note, store)
+      } else return Promise.reject('Invalid note')
+    },
+
+    editNote(note) {
+      if (checkNote(note)) {
+        return set(note.id, note, store)
+      } else return Promise.reject('Invalid note')
+    },
+
+    deleteNote(id) {
+      return del(id, store)
+    },
+  }
+}
